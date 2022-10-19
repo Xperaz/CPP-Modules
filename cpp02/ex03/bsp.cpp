fe64c9fe11775eac7e57790af00cbfcb47279aaf
Xperaz <aouhadou@student.1337.ma>
2022-10-19 19:24:31 +0100

    finish ec03

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,25 +6,35 @@
 /*   By: aouhadou <aouhadou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 10:09:53 by aouhadou          #+#    #+#             */
-/*   Updated: 2022/10/18 22:51:00 by aouhadou         ###   ########.fr       */
+/*   Updated: 2022/10/19 19:13:58 by aouhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-bool bsp( Point const a, Point const b, Point const c, Point const point)
+float   get_area(Point const a, Point const b, Point const c)
 {
-    std::cout << point.getx() << std::endl;
+    float a1 = (c.getx() * (a.gety() - b.gety()));
+    float a2 = (a.getx() * (b.gety() - c.gety()));
+    float a3 = (b.getx() * (c.gety() - a.gety()));
+    float area =  (a1 + a2 + a3) / 2; 
+    if (area < 0)
+        area *= -1;
+    return (area);
+}
 
-    float area = 1 / 2 * fabs((a.getx() * (b.gety() - c.gety())) + (b.getx() * (c.gety() - a.gety()))
-                + (c.getx() * (a.gety() - b.gety())));
-
-    float a1 = fabs((c.getx() * (a.gety() - b.gety())));
-    float a2 = fabs((a.getx() * (b.gety() - c.gety())));
-    float a3 = fabs((b.getx() * (c.gety() - a.gety())));
-    float res =  (a1 + a2 + a3) / 2; 
-    std::cout << area << "\n" <<  res << std::endl;
+bool bsp( Point const a, Point const b, Point const c, Point const point)
+{ 
+    float full_area = get_area(a, b, c);
     
+    float area1 = get_area(point, a, c);
+    float area2 = get_area(point, a, b);
+    float area3 = get_area(point, b, c);
+    
+    float add = area1 + area2 + area3;
+    //std::cout << full_area << "\n" << add << std::endl;
+    if (add == full_area)
+        return (1);
     return (0);
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aouhadou <aouhadou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 16:34:16 by aouhadou          #+#    #+#             */
-/*   Updated: 2022/10/18 22:42:49 by aouhadou         ###   ########.fr       */
+/*   Updated: 2022/10/19 19:24:12 by aouhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,8 @@
 #include "Point.hpp"
 
 int main( void ) {
-    // Fixed a;
     
-    // Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    // std::cout << a << std::endl;
-    // std::cout << ++a << std::endl;
-    // std::cout << a << std::endl;
-    // std::cout << a++ << std::endl;
-    // std::cout << a << std::endl;
-    // std::cout << b << std::endl;
-    // std::cout << Fixed::max( a, b ) << std::endl;
-
-    Point a(1.0, -2.0), b(10.0, 2.0), c(2.0, 4.0), myPoint(1.0, 3.0);
+    Point a(1.0, -2.0), b(10.0, 2.0), c(1.0, 4.0), myPoint(2, 2);
 
     bool bsp_ = bsp(a, b, c, myPoint);
     std::cout << bsp_ <<  " " << std::endl;
