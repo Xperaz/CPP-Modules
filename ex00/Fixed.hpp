a43f2a70ed6d7d6f89030ca406b0f2e746a01ed6
Xperaz <aouhadou@student.1337.ma>
2022-10-21 17:08:32 +0100

    fix ex03

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouhadou <aouhadou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 16:23:57 by aouhadou          #+#    #+#             */
+/*   Updated: 2022/10/20 16:23:58 by aouhadou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+
+	public:
+
+		Fixed();
+		Fixed( Fixed const & src );
+		~Fixed();
+
+		int			getRawBits( void ) const;
+		void		setRawBits( int const raw );
+		Fixed &		operator=( Fixed const & rhs );
+
+	private:
+		int _fp;
+		static const int bits = 8;
+
+};
+
+std::ostream &			operator<<( std::ostream & o, Fixed const & i );
+
+#endif
