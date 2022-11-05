14a25fde849221296bfc96b2e4fa096161461a88
Xperaz <aouhadou@student.1337.ma>
2022-11-05 10:26:58 +0100

    add ex02

---
+++ cpp06/ex02/Base.hpp
@@ -0,0 +1,17 @@
+#ifndef BASE_HPP
+# define BASE_HPP
+
+# include <iostream>
+# include <string>
+
+class Base
+{
+
+	public:
+		virtual ~Base();
+	private:
+
+};
+
+#endif 
+/* ************************************************************ BASE_H */
---
+++ cpp06/ex02/Identify.cpp
@@ -0,0 +1,1 @@
+#include "Identify.hpp"
