4744c137d099e5e4d9f9b861365bac14c8d68a99
Azedine Ouhadou <aouhadou@student.1337.ma>
2023-03-21 10:52:04 +0000

    complete ex01

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,5 +1,12 @@
 #include "RPN.hpp"
 
+bool IsStrOperator(std::string str)
+{
+    if (str == "/" || str == "*" || str == "+" || str == "-")
+        return (true);
+    return (false);
+}
+
 bool IsOperator(char c)
 {
     if (c == '/' || c == '*' || c == '+' || c == '-')
@@ -30,6 +37,11 @@ bool CheckInput(int ac, char **av)
     return (true);
 }
 
+double Multiplication(double a, double b){ return (a * b); }
+double Adding(double a, double b){ return (a + b); }
+double Substraction(double a, double b){ return (a - b); }
+double Division(double a, double b){ return (a / b); }
+
 int main(int ac, char **av)
 {
     if (!CheckInput(ac, av))
@@ -37,9 +49,67 @@ int main(int ac, char **av)
     std::string str(av[1]);
     std::stringstream ss(str);
     std::string token;
+    std::stack<int> myStack;
     while (getline(ss, token, ' ')) {
-        std::stack<int> myStack;
+        if (!IsStrOperator(token))
+            myStack.push(atoi((char *)token.c_str()));
+        else if (IsStrOperator(token) && myStack.size() > 1)
+        {
+            if (token == "*")
+            {
+                double a, b, c;
+                a = myStack.top();
+                myStack.pop();
+                b = myStack.top();
+                myStack.pop();
+                c = Multiplication(b, a);
+                myStack.push(c);
+                continue;
+            }
+
+            if (token == "/")
+            {
+                double a, b, c;
+                a = myStack.top();
+                myStack.pop();
+                b = myStack.top();
+                myStack.pop();
+                c = Division(b, a);
+                myStack.push(c);
+                continue;
+            }
+
+            if (token == "+")
+            {
+                double a, b, c;
+                a = myStack.top();
+                myStack.pop();
+                b = myStack.top();
+                myStack.pop();
+                c = Adding(b, a);
+                myStack.push(c);
+                continue;
+            }
+
+            if (token == "-")
+            {
+                double a, b, c;
+                a = myStack.top();
+                myStack.pop();
+                b = myStack.top();
+                myStack.pop();
+                c = Substraction(b, a);
+                myStack.push(c);
+                continue;
+            }
+         }
+         else
+         {
+            std::cout << "argument is not Reverse Polish Notation" << std::endl;
+            return (0);
+         }
     }
-    
+    std::cout << myStack.top() << std::endl;
+    myStack.pop();
     return (0);
 } 
